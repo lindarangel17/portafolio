57f5b7c263cfb7fe5351c65b767fd7649a8b6d79
lindarangel17 <62438084+lindarangel17@users.noreply.github.com>
2021-04-25 13:21:49 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+    $('#a-contacto, #a-contacto-movil').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000,'easeOutExpo');
+        event.preventDefault();
+    });
+
+    // Abrir y Cerrar Menú Responsive >>> Izquierda 290px
+    $('#abrir').click(function(){
+        $('#menu-responsive').animate({
+            left: 0
+        });
+        $('#abrir').hide();
+        $('#cerrar').show();
+        event.preventDefault();
+    });
+
+    $('#cerrar, #a-contacto-movil').click(function(){
+        $('#menu-responsive').animate({
+            left: -290
+        });
+        $('#cerrar').hide();
+        $('#abrir').show();
+        event.preventDefault();
+    });
